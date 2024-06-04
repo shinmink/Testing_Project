f08262019ccdc0acb80dcea65635229e3f9ead22
원지연 <dnjs229@gmail.com>
2024-06-04 14:30:06 +0900

    feat: init

---
+++ src/main/java/com/sparta/wildcard_newsfeed/WildcardNewsfeedApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.wildcard_newsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildcardNewsfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildcardNewsfeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/wildcard_newsfeed/WildcardNewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.wildcard_newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildcardNewsfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
