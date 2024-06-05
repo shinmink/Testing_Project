b411b2aef8666fa2912ddfa877c7b655dfdd0fe1
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-06-05 17:24:17 +0900

    refactor: 게시물 수정, 삭제 v1.0.5 토큰 검사 삭제, @AuthenticationPrincipal AuthenticationUser user 활용

--- src/main/java/com/sparta/wildcard_newsfeed/domain/post/controller/PostController.java
+++ src/main/java/com/sparta/wildcard_newsfeed/domain/post/controller/PostController.java
@@ -5,12 +5,13 @@ import com.sparta.wildcard_newsfeed.domain.post.dto.PostRequestDto;
 import com.sparta.wildcard_newsfeed.domain.post.dto.PostResponseDto;
 import com.sparta.wildcard_newsfeed.domain.post.service.PostService;
 import com.sparta.wildcard_newsfeed.domain.user.entity.User;
-import jakarta.servlet.http.HttpServletRequest;
+import com.sparta.wildcard_newsfeed.security.AuthenticationUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -53,8 +54,11 @@ public class PostController {
     //게시물 수정
     @PutMapping("/{postId}")
     public ResponseEntity<CommonResponseDto<PostResponseDto>> updatePost(
-            @Valid @RequestBody PostRequestDto postRequestDto, @PathVariable Long postId, HttpServletRequest request) {
-        PostResponseDto postResponseDto = postService.updatePost(postRequestDto, postId, request);
+            @AuthenticationPrincipal AuthenticationUser user,
+            @Valid @RequestBody PostRequestDto postRequestDto,
+            @PathVariable Long postId
+    ) {
+        PostResponseDto postResponseDto = postService.updatePost(postRequestDto, postId, user);
         return ResponseEntity.ok()
                 .body(CommonResponseDto.<PostResponseDto>builder()
                         .statusCode(HttpStatus.OK.value())
@@ -66,8 +70,10 @@ public class PostController {
     //게시물 삭제
     @DeleteMapping("/{postId}")
     public ResponseEntity<CommonResponseDto<PostResponseDto>> deletePost(
-            @Valid @PathVariable Long postId, HttpServletRequest request) {
-        postService.deletePost(postId, request);
+            @AuthenticationPrincipal AuthenticationUser user,
+            @Valid @PathVariable Long postId
+    ) {
+        postService.deletePost(postId, user);
         return ResponseEntity.ok()
                 .body(CommonResponseDto.<PostResponseDto>builder()
                         .statusCode(HttpStatus.OK.value())
