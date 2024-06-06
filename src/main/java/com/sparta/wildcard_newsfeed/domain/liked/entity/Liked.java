65390a2f6bb5190b3bf6aed3a5cf8c7f3b2078a2
원지연 <dnjs229@gmail.com>
2024-06-06 11:35:01 +0900

    refactor: Liked Entity의 컬럼 이름 변경 - contestsTypeEnum 에서 contestsType으로

--- src/main/java/com/sparta/wildcard_newsfeed/domain/liked/entity/Liked.java
+++ src/main/java/com/sparta/wildcard_newsfeed/domain/liked/entity/Liked.java
@@ -23,5 +23,5 @@ public class Liked extends TimeStampEntity {
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private ContentsTypeEnum contentsTypeEnum;
+    private ContentsTypeEnum contentsType;
 }
